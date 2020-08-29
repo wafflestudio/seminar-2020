2c0aad32500a35220a70515ffd1cee29450f3b87
Davin <bdv111@snu.ac.kr>
2020-08-29 12:58:33 +0900

    backend seminar 0

---
+++ backend/seminar0/oop/intro.py
@@ -0,0 +1,7 @@
+hero_name = '아이언맨'
+hero_health = 100
+hero_damage = 200
+hero_inventory = [
+    {'gold': 500},
+    {'weapon': '레이저'}
+]
---
+++ backend/seminar0/oop/not_oop.py
@@ -0,0 +1,53 @@
+# 히어로 1
+hero_1_name = '아이언맨'
+hero_1_health = 100
+hero_1_damage = 200
+hero_1_inventory = [
+    {'gold': 500},
+    {'weapon': '레이저'}
+]
+
+# 히어로 2
+hero_2_name = '데드풀'
+hero_2_health = 300
+hero_2_damage = 30
+hero_2_inventory = [
+    {'gold': 300},
+    {'weapon': '장검'}
+]
+
+# 히어로 3
+hero_3_name = '울버린'
+hero_3_health = 200
+hero_3_damage = 50
+hero_3_inventory = [
+    {'gold': 350},
+    {'weapon': '클로'}
+]
+
+# 몬스터 1
+monster_1_name = '고블린'
+monster_1_health = 90
+monster_1_damage = 30
+monster_1_inventory = [
+    {'gold': 50},
+    {'weapon': '창'}
+]
+
+# 몬스터 2
+monster_2_name = '드래곤'
+monster_2_health = 200
+monster_2_damage = 80
+monster_2_inventory = [
+    {'gold': 200},
+    {'weapon': '화염'}
+]
+
+# 몬스터 3
+monster_3_name = '뱀파이어'
+monster_3_health = 80
+monster_3_damage = 120
+monster_3_inventory = [
+    {'gold': 1000},
+    {'weapon': '최면술'}
+]
