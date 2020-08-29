4a1b21b735364d92407e21bfb3336f75c8eee2dc
Davin <bdv111@snu.ac.kr>
2020-08-29 16:08:35 +0900

    minor changes for backend seminar0

--- backend/seminar0/waffle_backend/survey/views.py
+++ backend/seminar0/waffle_backend/survey/views.py
@@ -10,10 +10,12 @@ class SurveyViewSet(viewsets.GenericViewSet):
     queryset = SurveyResult.objects.all()
     serializer_class = SurveyResultSerializer
 
+    # GET /api/v1/survey/
     def list(self, request):
         surveys = self.get_queryset()
         return Response(self.get_serializer(surveys, many=True).data)
 
+    # GET /api/v1/survey/{survey_id}/
     def retrieve(self, request, pk=None):
         survey = get_object_or_404(SurveyResult, pk=pk)
         return Response(self.get_serializer(survey).data)
