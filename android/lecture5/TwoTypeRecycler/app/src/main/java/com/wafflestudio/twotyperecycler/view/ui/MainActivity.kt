98b3760596f7f0e5d427a1ad7a5946593c6db415
Ion Jo <onemonsterceo@gmail.com>
2020-10-30 01:44:36 +0900

    TwoTypeAdapter

--- android/lecture5/TwoTypeRecycler/app/src/main/java/com/wafflestudio/twotyperecycler/view/ui/MainActivity.kt
+++ android/lecture5/TwoTypeRecycler/app/src/main/java/com/wafflestudio/twotyperecycler/view/ui/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.wafflestudio.twotyperecycler.R
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,17 +19,33 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private val compositeDisposable = CompositeDisposable()
 
+    private lateinit var adapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeViews()
 
         viewModel.getTodos()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                Log.d("-_-_-", it.toString())
+                adapter.todos = it
+                adapter.notifyDataSetChanged()
             }, {
-                Log.d("-_-_-", "getTodos error", it)
+                Log.d(MainActivity::class.java.name, "getTodos error", it)
             })
+    }
+
+    private fun initializeViews() {
+        adapter = TodoAdapter()
+        todo_list.layoutManager = LinearLayoutManager(this)
+        todo_list.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
